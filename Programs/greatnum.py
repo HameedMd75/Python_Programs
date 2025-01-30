acf7926347226920ccc56f17430a65ab9289e7ff
Mohammed Hameeduddin Ahmed <hameed9436@gmail.com>
2025-01-30 18:22:32 +0530

    Create greatnum.py

---
+++ Programs/greatnum.py
@@ -0,0 +1,10 @@
+a=float(input("a="))
+b=float(input("b="))
+c=float(input("c="))
+if a>b and c<a:
+  print("a is greater")
+elif b>a and c<b:
+  print("b is greatest")
+else:
+  print("c is great")
+
